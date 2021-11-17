2e4f8bc094f496a79716b23a56a3a0132d826f24
artdevil <artdvl91@gmail.com>
2021-11-17 12:24:21 +0700

    update ransack

--- lib/ransack_ext.rb
+++ lib/ransack_ext.rb
@@ -10,6 +10,16 @@ Ransack.configure do |config|
     config.add_predicate old, arel_predicate: current
   end
 
+  config.add_predicate 'gteq_date',
+                        arel_predicate: 'gteq',
+                        formatter: proc { |v| v.beginning_of_day },
+                        type: :date
+
+  config.add_predicate 'lteq_date',
+                        arel_predicate: 'lteq',
+                        formatter: proc { |v| v.end_of_day },
+                        type: :date
+
   config.add_predicate "gteq_datetime",
                        arel_predicate: "gteq",
                        formatter: ->(v) { v.beginning_of_day }
